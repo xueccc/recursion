5dc3f93b2ab3f1633b2d36d717f5750740ef4862
Xue Chen <xueqingc@gmail.com>
2018-04-09 00:02:20 -0700

    add function definition for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,65 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // your code goes her
+
+  if (typeof obj === "undefined"){
+    return //undefined;
+  }
+
+  else if (obj === null){
+    return "" + null + "";
+  }
+  else if (typeof obj === "boolean"){
+    return "" + obj + "";
+  }
+
+  else if (typeof obj === "number"){
+     obj =  "" + obj;
+    return obj;
+  }
+  
+   else if (typeof obj === "string"){
+    return '"' + obj + '"';
+   }
+
+   else if (obj === {}){
+     return '{}';
+   }
+
+   else if (typeof obj  === "function"){
+     return obj;
+   }
+
+   else if (Array.isArray(obj) && obj.length === 0){
+    return "[]";
+   }
+
+   else if (Array.isArray(obj)){
+    var arr = obj.map(function(item){
+     return item = stringifyJSON(item);
+    })
+
+    return "[" + arr.join(",") + "]";
+   }
+
+
+   else if (typeof obj === "object"){
+    var result = [];
+    Object.keys(obj).forEach(function(key){
+
+      var val = stringifyJSON(obj[key]);
+      if (val === undefined || typeof val === "function"){
+        return {};
+      } else {
+      
+      result.push( '"' + key + '":' + val);
+      }
+    
+     });
+    return "{" + result.join(",")+ "}";
+   }
+  
+  
 };
+
